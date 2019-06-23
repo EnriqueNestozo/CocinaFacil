cde776ebf72b879a5722001dd3b5b1e6f8173e73
Enrique <enrique.flores093@gmail.com>
2019-06-22 19:57:59 -0500

    Creation of cocinaFacil project which is implemented using MVP pattern

---
+++ app/src/main/java/com/nestozo/enriq/cocinafacil/Autenticacion/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nestozo.enriq.cocinafacil.Autenticacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nestozo.enriq.cocinafacil.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
